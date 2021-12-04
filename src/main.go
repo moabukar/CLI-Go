12ad3fae8c8f5dd4a781d037939393bb0db17850
moabukar <mo-abukar10@hotmail.com>
2021-12-04 20:50:24 +0000

    adding subcommands

--- src/main.go
+++ src/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"flag"
+)
+
 func main() {
+	// subcommand = "videos get"
+	getCmd := flag.NewFlagSet("get", flag.ExitOnError)
+
+	// subcommand for video get all & ID
+	getAll := getCmd.Bool("all", false, "Get all videos")
+	getID := getCmd.String("id", "", "Video ID")
+
+	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
+
+	//
+	add := addCmd.String()
 
 }
