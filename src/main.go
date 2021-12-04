36ba4d80843e7e2a3c2316ce8a175c0ec432af13
moabukar <mo-abukar10@hotmail.com>
2021-12-04 20:57:37 +0000

    add subcommands

--- src/main.go
+++ src/main.go
@@ -15,6 +15,9 @@ func main() {
 	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
 
 	//
-	add := addCmd.String()
-
+	addID := addCmd.String("id", "", "Video ID")
+	addTitle := addCmd.String("title", "", "Video Title")
+	addUrl := addCmd.String("Url", "", "Video URL")
+	addImageUrl := addCmd.String("iamge URL", "", "video image URL")
+	addDesc := addCmd.String("desc", "", "video description")
 }
