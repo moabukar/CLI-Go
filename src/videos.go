3d19ddcf89c5bfd589044a3dc9102e5ba17d9808
moabukar <mo-abukar10@hotmail.com>
2021-12-04 20:40:51 +0000

    add getVideo + saveVideo functions

--- src/videos.go
+++ src/videos.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+type video struct {
+	Id          string
+	Title       string
+	Description string
+	Imageurl    string
+	Url         string
+}
+
+func getVideos()(video []video){
+
+	fileBytes, err !: ioutil.ReadFile("./videos.json")
+
+	if err != nil {    //error handling here
+		panic(err)
+	}
+
+	err = json.Unmarshal(fileBytes, &videos)
+
+	if err!= nil {
+		panic(err)
+	}
+
+	return videos
+}
+
+func saveVideos()(video []video){
+
+	videoBytes, err := json.Marshal(videos)
+
+	if err != nil {
+		panic(err)
+	}
+	
+	err = ioutil.WriteFile("./videos.json",videoBytes,0644)
+	if err != nil {
+		panic(err)
+	}
+}
